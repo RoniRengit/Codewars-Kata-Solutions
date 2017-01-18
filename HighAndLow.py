248f05322a75189710d1b6cc1f0d1c2ff67ea8b4
Roni Rengit <RoniRengit@users.noreply.github.com>
2017-01-18 23:00:14 +0400

    Updated Description

--- HighAndLow.py
+++ HighAndLow.py
@@ -1,3 +1,18 @@
+"""
+Description:
+In this little assignment you are given a string of space separated numbers, 
+and have to return the highest and lowest number.
+Author: Roni Rengit
+"""
+
+# My Final Solution
+def high_and_low(numbers):
+    """Function to get min and max from list"""
+    
+    tmp = [int(i) for i in numbers.split(' ')] # Create a list of integers
+    return str(max(tmp)) + ' ' +  str(min(tmp))
+
+# Another Solution
 def high_and_low(numbers):
     tmp = numbers.split(' ')
     tmp = [int(i) for i in tmp]
@@ -7,10 +22,5 @@ def high_and_low(numbers):
     string += str(tmp.pop(0))
     return string
 
-def high_and_low(numbers):
-    tmp = numbers.split(' ')
-    tmp = [int(i) for i in tmp]
-    tmp = str(min(tmp)) + ' ' +  str(max(tmp))
-    return tmp
-
+# Test Case
 print(high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"))

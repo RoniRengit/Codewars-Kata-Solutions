f828b054b2da20730bd3e668bc56b162400a214b
Roni Rengit <RoniRengit@users.noreply.github.com>
2017-01-18 23:03:50 +0400

    Updated Description

--- TwoPlayOneWin.py
+++ TwoPlayOneWin.py
@@ -1,3 +1,11 @@
+"""
+Create a function that returns the name of the winner in a fight between two fighters.
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+Author: Roni Rengit
+"""
 class Fighter(object):
     def __init__(self, name, health, damage_per_attack):
         self.name = name
@@ -25,5 +33,5 @@ def declare_winner(fighter1, fighter2, first_attacker):
     else:
         return attacker1[0].name
 
-
+# Test Case
 print(declare_winner(Fighter("Jerry", 30, 5), Fighter("Harald", 20, 5), "Harald"))
